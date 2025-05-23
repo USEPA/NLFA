bdf209dfd7e93b7a53938608c15c7acc88568f0b
skocher42 <kocher.scott@gmail.com>
2025-05-23 13:11:45 -0400

    Add files via upload

---
+++ database/nlfwa_web/synonyms.sql
@@ -0,0 +1,273 @@
+--------------------------------------------------------
+--  File created - Friday-May-23-2025   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Synonymn ACTIVE_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ACTIVE_ADVISORIES" FOR "NLFWA"."ACTIVE_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn ACTIVE_ADVISORIES_BY_POP
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ACTIVE_ADVISORIES_BY_POP" FOR "NLFWA"."ACTIVE_ADVISORIES_BY_POP";
+--------------------------------------------------------
+--  DDL for Synonymn ACTIVE_REGIONAL_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ACTIVE_REGIONAL_ADVISORIES" FOR "NLFWA"."ACTIVE_REGIONAL_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn ACTIVE_STATEWIDE_SYMBOLOGY
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ACTIVE_STATEWIDE_SYMBOLOGY" FOR "NLFWA"."ACTIVE_STATEWIDE_SYMBOLOGY";
+--------------------------------------------------------
+--  DDL for Synonymn ADV_TYPES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ADV_TYPES" FOR "NLFWA"."ADV_TYPES";
+--------------------------------------------------------
+--  DDL for Synonymn ADVDET_V
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ADVDET_V" FOR "NLFWA"."ADVDET_V";
+--------------------------------------------------------
+--  DDL for Synonymn ADVDET2_V
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ADVDET2_V" FOR "NLFWA"."ADVDET2_V";
+--------------------------------------------------------
+--  DDL for Synonymn ADVISORY
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ADVISORY" FOR "NLFWA"."ADVISORY";
+--------------------------------------------------------
+--  DDL for Synonymn ADVISORY_INFO
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ADVISORY_INFO" FOR "NLFWA"."ADVISORY_INFO";
+--------------------------------------------------------
+--  DDL for Synonymn ALL_FISH_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ALL_FISH_ADVISORIES" FOR "NLFWA"."ALL_FISH_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn ALL_REGIONAL_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ALL_REGIONAL_ADVISORIES" FOR "NLFWA"."ALL_REGIONAL_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn ALL_STATEWIDE_SYMBOLOGY
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ALL_STATEWIDE_SYMBOLOGY" FOR "NLFWA"."ALL_STATEWIDE_SYMBOLOGY";
+--------------------------------------------------------
+--  DDL for Synonymn ALL_TISSUE_STATIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."ALL_TISSUE_STATIONS" FOR "NLFWA"."ALL_TISSUE_STATIONS";
+--------------------------------------------------------
+--  DDL for Synonymn BLUEGILL_SUNFISH_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."BLUEGILL_SUNFISH_ADVISORIES" FOR "NLFWA"."BLUEGILL_SUNFISH_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn CHLORDANE_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."CHLORDANE_ADVISORIES" FOR "NLFWA"."CHLORDANE_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn COMADV
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."COMADV" FOR "NLFWA"."COMADV";
+--------------------------------------------------------
+--  DDL for Synonymn COMADVDET
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."COMADVDET" FOR "NLFWA"."COMADVDET";
+--------------------------------------------------------
+--  DDL for Synonymn DDT_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."DDT_ADVISORIES" FOR "NLFWA"."DDT_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn DETAILS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."DETAILS" FOR "NLFWA"."DETAILS";
+--------------------------------------------------------
+--  DDL for Synonymn DETAILS_V
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."DETAILS_V" FOR "NLFWA"."DETAILS_V";
+--------------------------------------------------------
+--  DDL for Synonymn DIOXIN_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."DIOXIN_ADVISORIES" FOR "NLFWA"."DIOXIN_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn LAKE_TROUT_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."LAKE_TROUT_ADVISORIES" FOR "NLFWA"."LAKE_TROUT_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn LARGEMOUTH_BASS_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."LARGEMOUTH_BASS_ADVISORIES" FOR "NLFWA"."LARGEMOUTH_BASS_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn LUT_DATAQUALIFIERS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."LUT_DATAQUALIFIERS" FOR "NLFWA"."LUT_DATAQUALIFIERS";
+--------------------------------------------------------
+--  DDL for Synonymn MEAL_ADVICE
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."MEAL_ADVICE" FOR "NLFWA"."MEAL_ADVICE";
+--------------------------------------------------------
+--  DDL for Synonymn MERCURY_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."MERCURY_ADVISORIES" FOR "NLFWA"."MERCURY_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn METHOD
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."METHOD" FOR "NLFWA"."METHOD";
+--------------------------------------------------------
+--  DDL for Synonymn NLFWA
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."NLFWA" FOR "NLFWA"."NLFWA";
+--------------------------------------------------------
+--  DDL for Synonymn PCB_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."PCB_ADVISORIES" FOR "NLFWA"."PCB_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn POLLUTANT
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."POLLUTANT" FOR "NLFWA"."POLLUTANT";
+--------------------------------------------------------
+--  DDL for Synonymn POPULATION
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."POPULATION" FOR "NLFWA"."POPULATION";
+--------------------------------------------------------
+--  DDL for Synonymn POPULATION_MAPPING
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."POPULATION_MAPPING" FOR "NLFWA"."POPULATION_MAPPING";
+--------------------------------------------------------
+--  DDL for Synonymn REGIONAL_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."REGIONAL_ADVISORIES" FOR "NLFWA"."REGIONAL_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn RESULTS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."RESULTS" FOR "NLFWA"."RESULTS";
+--------------------------------------------------------
+--  DDL for Synonymn SAMPLES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."SAMPLES" FOR "NLFWA"."SAMPLES";
+--------------------------------------------------------
+--  DDL for Synonymn SMALLMOUTH_BASS_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."SMALLMOUTH_BASS_ADVISORIES" FOR "NLFWA"."SMALLMOUTH_BASS_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn SMPL_TYPES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."SMPL_TYPES" FOR "NLFWA"."SMPL_TYPES";
+--------------------------------------------------------
+--  DDL for Synonymn SPECIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."SPECIES" FOR "NLFWA"."SPECIES";
+--------------------------------------------------------
+--  DDL for Synonymn STATE_WATERBODIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."STATE_WATERBODIES" FOR "NLFWA"."STATE_WATERBODIES";
+--------------------------------------------------------
+--  DDL for Synonymn STATES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."STATES" FOR "NLFWA"."STATES";
+--------------------------------------------------------
+--  DDL for Synonymn STATEWIDE_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."STATEWIDE_ADVISORIES" FOR "NLFWA"."STATEWIDE_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn STATEWIDE_SYMBOLOGY
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."STATEWIDE_SYMBOLOGY" FOR "NLFWA"."STATEWIDE_SYMBOLOGY";
+--------------------------------------------------------
+--  DDL for Synonymn STATIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."STATIONS" FOR "NLFWA"."STATIONS";
+--------------------------------------------------------
+--  DDL for Synonymn TECHNICAL_MAP_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."TECHNICAL_MAP_ADVISORIES" FOR "NLFWA"."TECHNICAL_MAP_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn TISSUE_PARMS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."TISSUE_PARMS" FOR "NLFWA"."TISSUE_PARMS";
+--------------------------------------------------------
+--  DDL for Synonymn TISSUE_STATION_SYMBOLOGY
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."TISSUE_STATION_SYMBOLOGY" FOR "NLFWA"."TISSUE_STATION_SYMBOLOGY";
+--------------------------------------------------------
+--  DDL for Synonymn TISSUE_V
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."TISSUE_V" FOR "NLFWA"."TISSUE_V";
+--------------------------------------------------------
+--  DDL for Synonymn TISSUE_WATERBODIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."TISSUE_WATERBODIES" FOR "NLFWA"."TISSUE_WATERBODIES";
+--------------------------------------------------------
+--  DDL for Synonymn WALLEYE_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."WALLEYE_ADVISORIES" FOR "NLFWA"."WALLEYE_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn WATERBODIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."WATERBODIES" FOR "NLFWA"."WATERBODIES";
+--------------------------------------------------------
+--  DDL for Synonymn WATERBODY_ADVISORIES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."WATERBODY_ADVISORIES" FOR "NLFWA"."WATERBODY_ADVISORIES";
+--------------------------------------------------------
+--  DDL for Synonymn WEB
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."WEB" FOR "NLFWA"."WEB";
+--------------------------------------------------------
+--  DDL for Synonymn WEB_SESSION_RESULTS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."WEB_SESSION_RESULTS" FOR "NLFWA"."WEB_SESSION_RESULTS";
+--------------------------------------------------------
+--  DDL for Synonymn WEB_SESSIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE SYNONYM "NLFWA_WEB"."WEB_SESSIONS" FOR "NLFWA"."WEB_SESSIONS";
